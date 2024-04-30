7658dc2050ec42fd6c37936d4fe8d5f8209926d3
AliSiddique <alisiddique10@hotmail.com>
2024-04-30 21:54:17 +0100

    ADDA

--- src/lib/Templates.ts
+++ src/lib/Templates.ts
@@ -1,45 +1,63 @@
 export const Templates = [
     {
+        title: "Saasunderone Original",
+        description: "A template for your business.",
+        image: "/integrations/saasunderone-logo.webp",
+        link: "/templates/saasunderone",
+        slug: "saasunderone",
+        coming_soon: false
+    },
+    {
       title: "Job Board",
       description: "A job board template.",
       image: "/integrations/jobboard-logo.webp",
       link: "/templates/jobboard",
-      slug: "jobboard"
+      slug: "jobboard",
+      coming_soon: true
     },
     {
       title: "AI Template",
       description: "An AI template for your business.",
       image: "/integrations/openai.jpeg",
       link: "/templates/ai",
-      slug: "ai"
+      slug: "ai",
+      coming_soon: true
     },
     {
       title: "Directory Template",
       description: "A directory template for your business.",
       image: "/integrations/directory-logo.webp",
       link: "/templates/directory",
-      slug : "directory"
+      slug : "directory",
+      coming_soon: true
+
     },
     {
       title:"Enterprise Template",
       description:"An enterprise template for your business.",
       image:"/integrations/turbo-logo.svg",
       link:"/templates/enterprise",
-      slug:"enterprise"
+      slug:"enterprise",
+      coming_soon: true
+
     },
     {
       title:"Next Auth Template",
       description:"An authentication template for your business.",
       image:"/integrations/nextauth.png",
       link:"/templates/nextauth",
-      slug:"nextauth"
+      slug:"nextauth",
+      coming_soon: true
+
     },
     {
       title:"Clerk Template",
       description:"A clerk template for your business.",
       image:"/integrations/clerk.svg",
       link:"/templates/clerk",
-      slug:"clerk"
+      slug:"clerk",
+      coming_soon: true
+
     }
   
 ]
